030ebf743574fa22d4148d06ca4a3a933fac6579
Jose-17-11 <ariashdezl@outlook.com>
2026-08-13 23:23:36 -0600

    chore: setup cloudflare worker deploy and scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  adapter: cloudflare()
 });
